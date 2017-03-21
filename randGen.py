e4b69ba411aa48d486b83ddc6cbc487f3fdbaab2
Jeremy <jeremychristopherryan@gmail.com>
2017-03-20 21:40:22 -0400

    Added proper name generation

--- randGen.py
+++ randGen.py
@@ -15,8 +15,12 @@ def generate(cardType = TYPE_CREATURE):
         i += 1
         name.append(random.choice(name_list()))
     if cardType == TYPE_CREATURE:
-        if random.random() < 0.25:
+        if random.random() < 0.35:
+            name = []
+            name.append(random_name())
             name.append("the")
+            if random.random() < 0.4:
+                name.append(random.choice(adjective()))
         name.append(random.choice(noun_list()))
     string = ""
     for item in name:
@@ -30,16 +34,40 @@ def name_list():
     "aegis", "honor", "mystic", "barrier", "burst", "charge", "aether",
     "meltdown", "rift", "shock", "shockwave", "lightning", "thunder",
     "hell", "storm", "tide", "ancient", "renegade", "agility", "warp",
-    "aim", "shoot", "nature", "vengeance", "damage", "wrath", "sun",
+    "aim", "nature", "vengeance", "damage", "wrath", "sun",
     "shadow", "light", "dark", "destiny", "favor", "ruin", "fucking", "soul",
-    "Broke", "frozen"]
+    "Broke", "frozen", "earth"]
+
+def adjective():
+    return ["forgotten", "ancient", "frozen", "scornful", "vengeful",
+    "resilient", "crafty", "colossal", "ice", "fire", "earth", "brutal",
+    "fucking", "wandering", "black", "dark", "virtuous"]
 
 def noun_list():
-    return ["cat", "mage", "horror", "bears", "warrior", "soldier",
+    return ["cat", "mage", "horror", "bear", "warrior", "soldier",
     "shade", "angel", "demon", "elf", "elemental", "phoenix", "hero",
     "wizard", "dragon", "fairy", "hellkite", "horse", "leech", "troll",
     "giant", "griffin", "person", "golem", "shaman", "prophet", "siren",
-    "succubis", "hydra", "basilisk", "satyr", "minotaur", "jesus", "fish",
+    "succubis", "hydra", "basilisk", "satyr", "minotaur", "fish",
     "gargoyle", "wolf", "ooze", "protector", "goblin", "destroyer", "ape",
     "roc", "beast", "colossus", "titan", "dwarf", "sphinx", "ravager",
-    "hellhound", "rogue", "knight", "tiger", "unicorn", "eater"]
+    "hellhound", "rogue", "knight", "tiger", "unicorn", "eater", "king"]
+
+def random_name():
+    name = ""
+    consonants = ["b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p",
+    "r", "s", "t", "v", "w", "y", "z"]
+    sounds = ["br", "rd", "rn", "st", "tr", "lt", "pt", "mn", "ny", "gh", "sh",
+    "xx", "kk", "ss", "tt", "mm", "rr", "ll"]
+    vowels = ["a", "e", "i", "o", "u"]
+    if random.random() < 0.5:
+        name = random.choice(vowels)
+    for i in range(0, round(2*random.random() + 1)):
+        if i == 0 or random.random() < 0.5:
+            name = name + random.choice(consonants)
+        else:
+            name = name + random.choice(sounds)
+        name = name + random.choice(vowels)
+    if random.random() < 0.5:
+        name = name + random.choice(consonants)
+    return name
