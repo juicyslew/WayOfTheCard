6085500a72aa6dbee1a46c653867495cfc46dd4f
Jeremy <jeremychristopherryan@gmail.com>
2017-03-28 21:05:50 -0400

    Updated board object.

--- Game.py
+++ Game.py
@@ -160,7 +160,7 @@ class Game():
         self.screen = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
         self.screen.fill((0, 0, 255))
         self.board = Board(self.screen)
-        self.board.update_board(self.screen, player1, player2)
+        self.update_board()
 
         while(self.running):  #While the game is still running (Which is essentially While True)
             self.update_board()
@@ -200,6 +200,7 @@ class Game():
                     card.effect.activate(player1, player2, TRIGGER_END) # If the cards have a "End Turn" Trigger, then activate effect
                     player1.check_dead(player2) # Check if anything died
                     player2.check_dead(player1)
+                    self.update_board()
                 except AttributeError: # Attribute error check, in case activating the card didn't work due to not having the attributes necessary (player_card)
                     continue
             if self.check_game_end(player1, player2): # if the game ends, end the game_loop
@@ -230,6 +231,7 @@ class Game():
                     card.effect.activate(player2, player1, TRIGGER_END) # if card has end trigger, activate effect.
                     player1.check_dead(player2) # Check if anything died.
                     player2.check_dead(player1)
+                    self.update_board()
                 except AttributeError:
                     continue
             #break # Break out of player2 while loop
