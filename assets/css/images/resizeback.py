1e0c2fda0271f6b623a7b6c8e0a1a0c1054ddfe5
MJ McMillen <16mmcmillen.stem@gmail.com>
2017-04-30 20:05:57 -0400

    resizing background

--- assets/css/images/resizeback.py
+++ assets/css/images/resizeback.py
@@ -4,5 +4,5 @@ from PIL import Image
 
 if __name__ == '__main__':
     Im = Image.open("overlay1.png")
-    Im = Im.resize((int(Im.size[0]*1.25), int(Im.size[1]*1.25)))
+    Im = Im.resize((int(Im.size[0]*0.85), int(Im.size[1]*.85)))
     Im = Im.save("overlay1.png")
